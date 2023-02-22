58e02de2e475d0ea37e8a11a87db91a8d3b97dd7
bemarchant <bemarchant@uc.cl>
2023-02-22 15:11:24 -0300

    just-a-commit

--- earth.py
+++ earth.py
@@ -23,42 +23,120 @@ class Earth:
             return slot.get_slot_out()
         else:
              return self.matrix[row*self.size + col]
-    
-    def get_slots_by_neighboring(self, slot):
+    def get_neighbornumber_by_slot(self, slot, slot_neighbor):
+        if slot.row == slot_neighbor.row and slot.col - 1 == slot_neighbor.col:
+            return 1 
+        if slot.row - 1 == slot_neighbor.row and slot.col == slot_neighbor.col:
+            return 2
+        if slot.row == slot_neighbor.row and slot.col + 1 == slot_neighbor.col:
+            return 3
+        if slot.row  + 1 == slot_neighbor.row and slot.col == slot_neighbor.col:
+            return 4
+
+        if slot.row + 1 == slot_neighbor.row and slot.col - 1 == slot_neighbor.col:
+            return 5 
+        if slot.row - 1 == slot_neighbor.row and slot.col - 1 == slot_neighbor.col:
+            return 6
+        if slot.row - 1 == slot_neighbor.row and slot.col + 1 == slot_neighbor.col:
+            return 7
+        if slot.row + 1 == slot_neighbor.row and slot.col + 1 == slot_neighbor.col:
+            return 8
+
+        if slot.row - 2 == slot_neighbor.row and slot.col == slot_neighbor.col:
+            return 9
+        if slot.row + 2 == slot_neighbor.row and slot.col == slot_neighbor.col:
+            return 10
+        if slot.row == slot_neighbor.row and slot.col + 2 == slot_neighbor.col:
+            return 11
+        if slot.row == slot_neighbor.row and slot.col - 2 == slot_neighbor.col:
+            return 12
+    def get_slot_by_neighbornumber(self, slot, neighbornumber):
         row = slot.row
         col = slot.col
-        neighborhood = []
-        
-        neighbor_1 = self.get_slot_by_index(row,col - 1)
-        neighbor_2 = self.get_slot_by_index(row - 1,col)
-        neighbor_3 = self.get_slot_by_index(row,col + 1)
-        neighbor_4 = self.get_slot_by_index(row + 1,col)
-       
-        neighbor_5 = self.get_slot_by_index(row + 1,col - 1)
-        neighbor_6 = self.get_slot_by_index(row - 1,col - 1)
-        neighbor_7 = self.get_slot_by_index(row - 1,col + 1)
-        neighbor_8 = self.get_slot_by_index(row + 1,col + 1)
+        if neighbornumber == 1:
+            return self.get_slot_by_index(row,col - 1)
+        if neighbornumber == 2:
+            return self.get_slot_by_index(row - 1,col)
+        if neighbornumber == 3:
+            return self.get_slot_by_index(row,col + 1)
+        if neighbornumber == 4:
+            return self.get_slot_by_index(row + 1,col)
+        if neighbornumber == 5:
+            return self.get_slot_by_index(row + 1,col - 1)
+        if neighbornumber == 6:
+            return self.get_slot_by_index(row - 1,col - 1)
+        if neighbornumber == 7:
+            return self.get_slot_by_index(row - 1,col + 1)
+        if neighbornumber == 8:
+            return self.get_slot_by_index(row + 1,col + 1)
+        if neighbornumber == 9:
+            return self.get_slot_by_index(row - 2,col)
+        if neighbornumber == 10:
+            return self.get_slot_by_index(row + 2,col)
+        if neighbornumber == 11:
+            return self.get_slot_by_index(row,col + 2)
+        if neighbornumber == 12:
+            return self.get_slot_by_index(row,col - 2)
 
-        neighbor_9 = self.get_slot_by_index(row - 2,col)
-        neighbor_10 = self.get_slot_by_index(row,col + 2)
-        neighbor_11 = self.get_slot_by_index(row + 2,col)
-        neighbor_12 = self.get_slot_by_index(row,col - 2)
+    def get_slots_by_adyacent(self, slot, neighbor):
 
+        adyacent_neighborhood = []
+        if neighbor == 1:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 1))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 5))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 6))
+            return adyacent_neighborhood
+        if neighbor == 2:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 2))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 6))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 7))
+            return adyacent_neighborhood
+        if neighbor == 3:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 3))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 7))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 8))
+            return adyacent_neighborhood
+        if neighbor == 4:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 4))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 5))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 8))
+            return adyacent_neighborhood
+        if neighbor == 5:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 1))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 4))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 5))
+            return adyacent_neighborhood
+        if neighbor == 6:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 1))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 2))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 6))
+            return adyacent_neighborhood   
+        if neighbor == 7:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 2))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 3))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 7))
+            return adyacent_neighborhood
+        if neighbor == 8:
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 3))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 4))
+            adyacent_neighborhood.append(self.get_slot_by_neighbornumber(slot, 8))
+            return adyacent_neighborhood      
+
+    def get_slots_by_neighboring(self, slot):
         neighborhood = [
-        neighbor_1, 
-        neighbor_2, 
-        neighbor_3, 
-        neighbor_4, 
-        neighbor_5, 
-        neighbor_6, 
-        neighbor_7, 
-        neighbor_8,
-        neighbor_9,
-        neighbor_10,
-        neighbor_11,
-        neighbor_12
+            self.get_slot_by_neighbornumber(slot, 1),
+            self.get_slot_by_neighbornumber(slot, 2),
+            self.get_slot_by_neighbornumber(slot, 3),
+            self.get_slot_by_neighbornumber(slot, 4),
+            self.get_slot_by_neighbornumber(slot, 5),
+            self.get_slot_by_neighbornumber(slot, 6),
+            self.get_slot_by_neighbornumber(slot, 7),
+            self.get_slot_by_neighbornumber(slot, 8),
+            self.get_slot_by_neighbornumber(slot, 9),
+            self.get_slot_by_neighbornumber(slot, 10),
+            self.get_slot_by_neighbornumber(slot, 11),
+            self.get_slot_by_neighbornumber(slot, 12),
         ]
-
         return neighborhood
     
     def get_slots_by_component(self, component):
@@ -76,7 +154,6 @@ class Earth:
         for slot in empty_slots:
             print(f"x = {slot.row} y = {slot.col}")
             rnd = random.randint(1,4)
-            # slot.move(rnd)
         pass
     def do_second_motion(self):
         pass
@@ -86,8 +163,29 @@ class Earth:
         k_list = self.get_slots_by_component("K")
         
         for k in k_list:
-            k_neighboring = self.get_slots_by_neighboring(k)
+            neighboring = self.get_slots_by_neighboring(k)
+            s_neighboring = list(filter(lambda slot: (slot.component == "S"), neighboring))
+
+            candidates = []
+
+            if self.get_slot_by_neighbornumber(k, 1) in s_neighboring and len(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 1)))) > 1:
+                candidates.extend(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 1))))
+            if self.get_slot_by_neighbornumber(k, 2) in s_neighboring and len(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 2)))) > 1:
+                candidates.extend(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 2))))
+            if self.get_slot_by_neighbornumber(k, 3) in s_neighboring and len(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 3)))) > 1:
+                candidates.extend(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 3))))
+            if self.get_slot_by_neighbornumber(k, 4) in s_neighboring and len(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 4)))) > 1:
+                candidates.extend(list(filter(lambda slot: (slot.component == "S"), self.get_slots_by_adyacent(k, 4))))
             
+            candidates = list(set(candidates))
+            if len(candidates) == 0:
+                return
+
+            rdn_slot_L = random.choice(candidates)
+            rdn_slot_S = random.choice(list(filter(lambda slot: (slot.component == "S" and slot != rdn_slot_L), self.get_slots_by_adyacent(k, self.get_neighbornumber_by_slot(k,rdn_slot_L)))))
+
+            rdn_slot_L.component = "L"
+            rdn_slot_S.component = "empty"
 
     def do_bond(self):
         pass
@@ -112,7 +210,7 @@ class Earth:
                 marker = "P"
                 color = 'k'
             elif(slot.component == "empty"):
-                marker = "bo"
+                marker = "x"
                 color = 'k'
-            plt.plot(slot.row, slot.col, marker, markersize=20, color = color)
+            plt.plot(slot.col, self.size - 1 - slot.row, marker, markersize=20, color = color)
         plt.show()
